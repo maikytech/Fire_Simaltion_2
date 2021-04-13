673728f0ee99f5861c126e27e634379a9a399ec6
Maiqui Cedeño <maikytech1307@gmail.com>
2021-04-13 18:21:23 -0500

    [Update] Floor scene

--- Assets/Project/Scripts/PuttingOutFire.cs
+++ Assets/Project/Scripts/PuttingOutFire.cs
@@ -4,36 +4,42 @@ using UnityEngine;
 
 public class PuttingOutFire : MonoBehaviour
 {
+    [SerializeField] float fireRate;
     [SerializeField] GameObject fire;
-    [SerializeField] GameObject fireExtinguisher;
+    [SerializeField] GameObject Flamethrower;
     [SerializeField] GameObject alarmaEvacuacion;
 
-    private AudioSource ExtinguisherAudio;
+    
+    private float nextFire;
+    //private AudioSource ExtinguisherAudio;
     private AudioSource audioAlarmaEvacuacion;
 
     private void Awake()
     {
-        ExtinguisherAudio = fireExtinguisher.GetComponent<AudioSource>();
         audioAlarmaEvacuacion = alarmaEvacuacion.GetComponent<AudioSource>();
+        nextFire = 0f;
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.CompareTag("Player"))
-        {
-            ExtinguisherAudio.Play();
-            Invoke("OutFire", 4.0f);
-            Invoke("AlarmaEvacuacion", 8.0f);
-        }
-    }
-
+    
     private void OutFire()
     {
         fire.SetActive(false);
-        ExtinguisherAudio.Stop();
+        //ExtinguisherAudio.Stop();
     }
     private void AlarmaEvacuacion()
     {
         Debug.Log("Se invoco la funcion");
         audioAlarmaEvacuacion.Stop();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("FireExtinguisher") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;            //Controla el delay de disparo.
+            Flamethrower.SetActive(true);
+
+            Invoke("OutFire", 21f);
+            Invoke("AlarmaEvacuacion", 25f);
+        }
     }
 }
